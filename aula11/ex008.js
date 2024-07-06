884c0286188820ead21fa62bbb3ecd941d682e77
Júnior Ottersbach <juniorott17@gmail.com>
2024-07-06 13:13:59 -0400

    dia 6 - js

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+console.log('-- Começo --')
+var vel = 59
+console.log(`A sua velocidade foi de ${vel}Km/h`)
+if (vel > 60) { //isso é uma condição simples
+    console.log('Você está acima da velocidade permitida, Multado.')
+}
+console.log('-- Fim --')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Bélgica'
+console.log(`Você está vivendo no seguinte país: ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro')
+} else {
+    console.log('Você é Estrangeiro')
+}
